0b556c36999affdcea28b803496cd3c64663761b
Mary Hughes <mhughes21@my.wctc.edu>
2020-04-13 11:19:05 -0500

    Week 1 Lab programs
    Program 1: Times Table Transform
    Program 2: Student and Teacher subclass creation

---
+++ MobileAppsWeek1/Program1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tableArray;
+            Console.WriteLine("Welcome to Mobile App-Program 1!");
+            // Asks user the the number between 3 and 9
+            Console.Write("Please enter a number between 3 and 9: ");
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                // Create a times table using that number
+                tableArray=CreateTimesTable(num);
+                TransformTimes[] xOperation = { new Horizontal(), new Vertical(), new DiagonalLeft(), new DiagonalRight() };
+                Boolean done = false;
+                while (!done)
+                {
+                    // Output a menu "Flip on 1) Horizon, 2) Vertical, 3) Diagonal Left, 4) Diagonal Right, 5) To End: "
+                    // I read the instructions as meaning transform the original table.
+                    // But the table itself is passed into the transform functions.
+                    // To transform the table in sequence, write the new transformed table into the 2D array
+                    // and update the transform methods to return the array.
+                    Console.WriteLine("Transform the table (will always transform the *original* table)");
+                    Console.WriteLine("Flip on ");
+                    Console.Write("  1) Horizon, 2) Vertical, 3) Diagonal Left, 4) Diagonal Right, 5) To End: ");
+                    if (int.TryParse(Console.ReadLine(), out int flipChoice))
+                    {
+                        if (flipChoice==5)
+                        {
+                            done = true;
+                        } else if (flipChoice > 0 && flipChoice < 5)
+                        {
+                            // Based upon selection, output the table with the new orientation, followed by the menu unless 5 is selected.
+                            xOperation[flipChoice - 1].OutputTransform(tableArray);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid flip choice.");
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid integer between 3 and 9.");
+            }
+
+        }
+
+        private static int[,] CreateTimesTable(int num)
+        {
+            int[,] tableArray = new int[num, num];
+            for (int i=0; i<num; i++)
+            {
+                for (int j=0; j<num; j++)
+                {
+                    tableArray[i, j] = (i+1) * (j+1);
+                    Console.Write($"{tableArray[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            return tableArray;
+        }
+    }
+
+    abstract class TransformTimes
+    {
+        public abstract void OutputTransform(int[,] tableArray);
+    }
+
+    class Horizontal: TransformTimes
+    {
+        override public void OutputTransform(int[,] tableArray)
+        {
+            int num = tableArray.GetLength(0);
+            Console.WriteLine("Outputting horizontal transform");
+            for (int i = num-1; i >= 0; i--)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write($"{tableArray[i,j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+    class Vertical : TransformTimes
+    {
+        override public void OutputTransform(int[,] tableArray)
+        {
+            int num = tableArray.GetLength(0);
+            Console.WriteLine("Outputting vertical transform");
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = num-1; j >= 0; j--)
+                {
+                    Console.Write($"{tableArray[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+
+    class DiagonalLeft : TransformTimes
+    {
+        override public void OutputTransform(int[,] tableArray)
+        {
+            int num = tableArray.GetLength(0);
+            Console.WriteLine("Outputting diagonal left transform (flipping around left-leaning diagonal)");
+            for (int j = 0; j < num; j++)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write($"{tableArray[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+
+    class DiagonalRight : TransformTimes
+    {
+        override public void OutputTransform(int[,] tableArray)
+        {
+            int num = tableArray.GetLength(0);
+            Console.WriteLine("Outputting diagonal right transform (flipping around forward slash diagonal)");
+            for (int j = num-1; j >= 0; j--)
+            {
+                for (int i = num - 1; i >= 0; i--)
+                {
+                    Console.Write($"{tableArray[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
+
---
+++ MobileAppsWeek1/Program2/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mobile Apps Program 2!");
+            //Program must include a single Collection.
+            List<Person> people = new List<Person>();
+
+            //Program asks user to enter the data for Students and teachers.
+            Boolean done = false;
+            while (!done)
+            {
+                Console.Write("Please enter a 1) Student; 2) Teacher; any other integer to end: ");
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice) {
+                        case 1:
+                            people.Add(new Student());
+                            break;
+                        case 2:
+                            people.Add(new Teacher());
+                            break;
+                        default:
+                            done = true;
+                            break;
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("Please enter a valid choice");
+                }
+            }
+            // Once all data is entered, a formatted listing of all Persons.
+            Console.WriteLine("List of people: ");
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+
+    enum Programs
+    {
+        None = 0,
+        ComputerScience, 
+        Accounting, 
+        English, 
+        Math,
+        Marketing,
+        Nursing
+    }
+    class Person
+    {
+        string name;
+        double age;
+        string id;
+
+        public Person()
+        {
+            Console.Write("Enter name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter age: ");
+            if (!Double.TryParse(Console.ReadLine(), out age)) 
+            {
+                age = 0;
+            }
+            Console.Write("Enter id: ");
+            id = Console.ReadLine();
+        }
+
+        override public String ToString()
+        {
+            return String.Format($"{id} {name} {age} ");
+        }
+    }
+
+    class Student: Person
+    {
+        int creditsEarned;
+        Programs program;
+
+        public Student(): base ()
+        {
+            string[] pgms = Enum.GetNames(typeof(Programs));
+            for (int i=0; i<pgms.Length; i++)
+            {
+                Console.WriteLine($"{i} - {pgms[i]}");
+            }
+            Console.Write("Enter degree program: ");
+            int programNum = 0;
+            if (!int.TryParse(Console.ReadLine(), out programNum))
+            {
+                programNum = 0;
+            }
+            program = (Programs) programNum;
+
+            Console.Write("Enter credits earned: ");
+            if (!int.TryParse(Console.ReadLine(), out creditsEarned))
+            {
+                creditsEarned = 0;
+            }
+        }
+        override public String ToString()
+        {
+            return base.ToString()+
+                String.Format($"{Enum.GetName(typeof(Programs), program)} - {creditsEarned} Credits Earned");
+        }
+
+    }
+
+    class Teacher: Person
+    {
+        int yearsOfService;
+        public Teacher(): base()
+        {
+            Console.Write("Enter years of service: ");
+            if (!int.TryParse(Console.ReadLine(), out yearsOfService))
+            {
+                yearsOfService = 0;
+            }
+        }
+        override public String ToString()
+        {
+            return base.ToString() + 
+                String.Format($" - {yearsOfService} Years of Service");
+        }
+
+    }
+
+}
